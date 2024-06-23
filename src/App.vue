f6cf4195a5fd34c96dc5d8fa9252729502eb0b72
Straka V <vladimir.strak@gmail.com>
2024-06-23 17:26:27 +0200

    Some css to show app and center it

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <script setup></script>
 
 <template>
-  <div>I am alive, don't you worry</div>
+  <div class="container mx-auto">
+    <div class="h-screen bg-slate-500">
+      <div>
+        <input placeholder="Add Todo" class="mx-2" /><button
+          class="border-black"
+        >
+          Add
+        </button>
+      </div>
+      <span class="mx-2"> I am alive, don't you worry </span>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
